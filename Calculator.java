d1ce4881a2b1dca6ca4629a39ed1c34839e38c75
Jerick Angelo Fider <jerickangelofider30@gmail.com>
2024-09-10 17:11:47 +0800

    Subtraction has been added

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,10 @@ public class Calculator{
     public int addition(int a , int b){
         return a+b;
     }
+
+    public int subtraction(int a, int b){
+        return a-b;
+    }
   
 
     public static void main(String[] args){
